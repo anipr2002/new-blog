d00eb6d51edc55114793521e76369b3470761430
Anirudh <anirudh.panchangamranganath@thws.study.de>
2023-09-07 04:40:29 +0200

    added .env

--- api/index.js
+++ api/index.js
@@ -1,124 +1,111 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import bycrypt from 'bcrypt';
-import User from './models/User.js';
-import jwt from 'jsonwebtoken';
-import cookieParser from 'cookie-parser';
-import multer from 'multer';
-import fs from 'fs';
-import path from 'path';
-import Post from './models/Post.js';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import bycrypt from "bcrypt";
+import User from "./models/User.js";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
+import multer from "multer";
+import fs from "fs";
+import path from "path";
+import Post from "./models/Post.js";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const salt = bycrypt.genSaltSync(10);
-const secret = 'dsgsldsfgkoewngrgopwn';
-const uploadMiddleware = multer({dest: 'uploads/'});
+const secret = "dsgsldsfgkoewngrgopwn";
+const uploadMiddleware = multer({ dest: "uploads/" });
 const app = express();
 
-
-app.use(cors({credentials: true, origin: 'http://localhost:5174'}));
+app.use(cors({ credentials: true, origin: "http://localhost:5174" }));
 app.use(express.json()); //middleware
-app.use(express.urlencoded({extended: true})); //middleware
+app.use(express.urlencoded({ extended: true })); //middleware
 app.use(cookieParser());
-app.use('/uploads', express.static(__dirname + '/uploads'));
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
+mongoose.connect(process.env.URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-mongoose.connect("mongodb+srv://blog:tBKuQB1e4pmBJjLp@cluster0.vfdzyb4.mongodb.net/?retryWrites=true&w=majority");
-
-
-
-
-
-app.post('/register', async (req, res) => {
-   const { username, password } = req.body;
-   try {
-    const userDoc= await User.create({
-        username, 
-        password: bycrypt.hashSync(password, salt)
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bycrypt.hashSync(password, salt),
     });
     res.json(userDoc);
-    } catch (error) {
-        res.status(400).json(error);
-    }
-
-    }
-);
-
-
-app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-    const userDoc = await User.findOne({username});
-    const passOk = bycrypt.compareSync(password, userDoc.password);
-    if(passOk){
-        //logged in
-        jwt.sign({username,id:userDoc._id},secret, {}, (err, token) => {
-            if(err) throw err;
-            res.cookie('token',token).json({
-                id: userDoc._id,
-                username,
-            });
-        });
-
-    }
-    else{
-        res.status(400).json('Wrong password');
-    }
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
-app.get('/profile', (req, res) => {
-    const { token } = req.cookies;
-    jwt.verify(token, secret, {}, (err, info) => {
-        if(err) throw err;
-        res.json(info);
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passOk = bycrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    //logged in
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie("token", token).json({
+        id: userDoc._id,
+        username,
+      });
     });
+  } else {
+    res.status(400).json("Wrong password");
+  }
 });
 
-app.post('/logout', (req, res) => {
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+});
 
-    res.cookie('token', '').json({message: 'Logged out'});
-}
-);
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json({ message: "Logged out" });
+});
 
+app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
+  const { originalname, path } = req.file;
+  const parts = originalname.split(".");
+  const ext = parts[parts.length - 1];
+  const newPath = path + "." + ext;
+  fs.renameSync(path, newPath);
 
-app.post('/post',uploadMiddleware.single('file'),async (req, res) => {
-    const { originalname,path} = req.file;
-    const parts = originalname.split('.');
-    const ext = parts[parts.length - 1];
-    const newPath = path + '.' + ext;
-    fs.renameSync(path, newPath);
-
-
-    const { token } = req.cookies;
-        jwt.verify(token, secret, {}, async (err,info) => {
-        if(err) throw err;
-        const {title, summary, content} = req.body;
-        const postDoc = await Post.create({
-            title,
-            summary,
-            content,
-            cover: newPath,
-            author: info.id,
-        });
-
-        res.json(postDoc);
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const { title, summary, content } = req.body;
+    const postDoc = await Post.create({
+      title,
+      summary,
+      content,
+      cover: newPath,
+      author: info.id,
     });
-    
 
+    res.json(postDoc);
+  });
 });
 
-app.get('/posts', async (req, res) => {
-    res.json(await Post.find()
-    .populate('author', ['username'])
-    .sort({createdAt: -1})
-    .limit(20));
-});  
-
-
+app.get("/posts", async (req, res) => {
+  res.json(
+    await Post.find()
+      .populate("author", ["username"])
+      .sort({ createdAt: -1 })
+      .limit(20)
+  );
+});
 
 app.listen(4000, () => {
-    console.log('Example app listening on port 4000!');
-    }
-);
+  console.log("Example app listening on port 4000!");
+});
